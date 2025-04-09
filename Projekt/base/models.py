afd5c4a21c0accb216978c6fc76177758034fc83
MartinKamera <martin.kamera@seznam.cz>
2025-04-09 21:20:16 +0000

    models rev.

--- Projekt/base/models.py
+++ Projekt/base/models.py
@@ -9,15 +9,30 @@ class FioCurr(models.Model):
     def __str__(self):
         return f"{self.symbol}"
 
+class Portfolio(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Transaction(models.Model):
+    portfolio = models.ForeignKey(Portfolio, related_name='transaction_history', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=3)
+    marketprice = models.DecimalField(max_digits=20, decimal_places=3,blank=True, null=True)
+    price = models.DecimalField(max_digits=20, decimal_places=3,blank=True, null=True)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.portfolio},{self.marketprice},{self.price}'
+
 class Coin(models.Model):
+    transaction = models.ForeignKey(Transaction, related_name='coin', on_delete=models.SET_NULL)
     id = models.CharField(max_length= 20, primary_key=True)
     symbol = models.CharField(max_length=10)
     name = models.CharField(max_length=30)
     marketpriceUSD = models.DecimalField(max_digits=20, decimal_places=3,blank=True, null=True)
     last_updated = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} at {self.marketpriceUSD} USD"
 
 class Price_history(models.Model):
     asset = models.ForeignKey(Coin, related_name='price_history', on_delete=models.CASCADE)
@@ -25,15 +40,4 @@ class Price_history(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f""
-
-class Portfolio(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-class Transaction(models.Model):
-    portfolio = models.ForeignKey(Portfolio, related_name='transaction_history', on_delete=models.CASCADE)
-    asset = models.ForeignKey(Coin, related_name='coin', on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=20, decimal_places=3)
-    marketprice = models.DecimalField(max_digits=20, decimal_places=3,blank=True, null=True)
-    price = models.DecimalField(max_digits=20, decimal_places=3,blank=True, null=True)
-    time = models.DateTimeField(auto_now_add=True)
+        return f'{self.asset} was {self.priceUSD} USD at {self.timestamp}'
