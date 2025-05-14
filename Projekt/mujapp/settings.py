ef10723e958310c807e0d134ef6c32bd475a3413
MartinKamera <martin.kamera@seznam.cz>
2025-05-14 16:42:37 +0000

    doufam

--- Projekt/mujapp/settings.py
+++ Projekt/mujapp/settings.py
@@ -80,10 +80,11 @@ WSGI_APPLICATION = "mujapp.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True  
+    )
 }
 
 # Password validation
