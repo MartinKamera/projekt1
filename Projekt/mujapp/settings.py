d7f1ac39c2f2ef95036c41e68750acb362ee7f2b
MartinKamera <martin.kamera@seznam.cz>
2025-05-14 16:15:18 +0000

    achjo

--- Projekt/mujapp/settings.py
+++ Projekt/mujapp/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 import dj_database_url
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -79,13 +82,17 @@ WSGI_APPLICATION = "mujapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#}
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
