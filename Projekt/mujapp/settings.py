93f8809e15bcab1bbdf314de1b0f917a6b381ee1
MartinKamera <martin.kamera@seznam.cz>
2025-05-15 04:57:10 +0000

    neon1

--- Projekt/mujapp/settings.py
+++ Projekt/mujapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-fupw2bo+q)m)^+(roa04z&(s&*pz$co$tpmdu(6w@9m(p_bf&y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
